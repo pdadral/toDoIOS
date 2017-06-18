214262c34a791c227247cf6087307fab9ef3bae3
Dadral <pdadral@gmail.com>
2017-06-18 23:38:37 +1200

    Add main View Controller and Tasks

---
+++ Todo/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  Todo
+//
+//  Created by Dadral on 18/06/17.
+//  Copyright © 2017 Dadral. All rights reserved.
+//
+
+import Foundation
+
+class Task{
+    var name = ""
+    var Important = false
+}
--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks :[Task] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tasks = makeTasks()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        
+        if task.Important{
+            cell.textLabel?.text = "❗️\(task.name)"
+        }
+        else{
+            cell.textLabel?.text = task.name
+        }
+        
+        
+        return cell
+    }
+    
+    func makeTasks() -> [Task]{
+        let task1 = Task()
+        task1.name = "Play Soccer"
+        task1.Important = false
+        
+        let task2 = Task()
+        task2.Important = true
+        task2.name = "Cooking"
+        return [task1,task2]
+    }
+    
+    @IBAction func plusTapped(_ sender: Any) {
+        performSegue(withIdentifier: "addSegue", sender: nil)
+    }
 }
 
